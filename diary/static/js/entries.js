f305f902e72f5ee25b553bd35a2203046a59720a
kingluko <kelvin.kitika@gmail.com>
2018-08-23 22:16:36 +0300

    Add view entry fetch

--- diary/static/js/entries.js
+++ diary/static/js/entries.js
@@ -17,7 +17,7 @@ fetch('https://andela-diaryapi.herokuapp.com/api/v1/entries', {
             const entry = entries[i];
             var row = `<tr>
                 <td>${entry.title}</td>
-                <td><a href="entry.html">View</a></td>
+                <td><a href="#" onclick="viewEntry(${entry.entry_id})">View</a></td>
                 <td><a href="new-entry.html">Modify</a></td>
                 <td><a href="">Delete</a></td>
               </tr>`;
@@ -25,3 +25,28 @@ fetch('https://andela-diaryapi.herokuapp.com/api/v1/entries', {
         }
         entriesTable.innerHTML = rows;
     });
+
+function viewEntry(entry_id) {
+    let url = 'https://andela-diaryapi.herokuapp.com/api/v1/entries/'+entry_id;
+    fetch(url, {
+        headers : {
+            'x-access-token': token,
+            'content-type': 'application/json'
+        }        
+    })
+        .then(response => response.json())
+        .then(data => {
+            let entry_id = data.entry[0].entry_id
+            let title = data.entry[0].title
+            let story = data.entry[0].story
+            let date_created = data.entry[0].date_created
+
+            localStorage.setItem('entry_id', entry_id)
+            localStorage.setItem('title', title);
+            localStorage.setItem('story', story);
+            localStorage.setItem('date_created', date_created);
+
+            window.location.replace('view_entry.html');
+        })
+        .catch(error => console.log(error));
+}
