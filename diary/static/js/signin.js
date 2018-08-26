504fb6345de23b4fb8615b28934e49872939d59d
kingluko <kelvin.kitika@gmail.com>
2018-08-26 21:45:33 +0300

    Modify fetch function

--- diary/static/js/signin.js
+++ diary/static/js/signin.js
@@ -1,29 +1,25 @@
 document.querySelector('.signin-form').addEventListener('submit', signIn)
 
 function signIn (event) {
-  event.preventDefault()
-  fetch('https://andela-diaryapi.herokuapp.com/api/v1/auth/signin', {
-    method: 'POST',
-    body: JSON.stringify({
-      username: document.querySelector('input[name=username]').value,
-      password: document.querySelector('input[name=password]').value
-    }),
-    headers: {
-      'content-type': 'application/json'
-    }
-  })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json()
-      } else {
-        document.querySelector('#signin-message').innerHTML = 'Login Failed! '
-      }
+    event.preventDefault()
+    fetch('https://andela-diaryapi.herokuapp.com/api/v1/auth/signin', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: document.querySelector('input[name=username]').value,
+            password: document.querySelector('input[name=password]').value
+        }),
+        headers: {
+            'content-type': 'application/json'
+        }
     })
-    .then(data => {
-      if (data.token) {
-        var token = data.token
-        localStorage.setItem('token', token)
-        window.location.href = 'diary-entries.html'
-      }
-    })
+        .then(response => response.json())
+        .then(data => {
+            let token = data.token;
+            localStorage.setItem('token', token);
+            document.querySelector('#message p').innerText = data.message;            
+            if (data.message === 'You have successfully logged in'){
+                window.location.assign('diary-entries.html');
+            }            
+        })
+        .catch(error => console.log(error));
 }
