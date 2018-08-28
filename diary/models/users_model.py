4887c83f9d83ec03c87d5a46155a279ea06d1a76
kingluko <kelvin.kitika@gmail.com>
2018-08-28 13:10:22 +0300

    Add User details to Users

--- diary/models/users_model.py
+++ diary/models/users_model.py
@@ -1,5 +1,6 @@
 from passlib.hash import sha256_crypt
 from diary.models.db import DbConnection
+from diary.models.entries_model import Entries
 
 db = DbConnection()
 
@@ -19,4 +20,19 @@ class Users:
             "INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
             (self.name, self.email, self.username, self.password))
 
+    @staticmethod
+    def user_details(user_id):
+        """This method return the user details"""
+        db.query(
+            "SELECT * FROM users WHERE id = %s", [user_id]
+        )
+        details = db.cur.fetchall()
+        entries = Entries.get(user_id)
+        display_details = {
+            'name': details[0][1],
+            'email': details[0][2],
+            'username': details[0][3],
+            'total_entries': len(entries)
+            }
+        return display_details
     
