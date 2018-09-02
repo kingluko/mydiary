c20f8f7c8c88f1dbfbedd8373fae072b573dd30e
kingluko <kelvin.kitika@gmail.com>
2018-09-02 15:33:17 +0300

    Add reminder function

--- diary/static/js/profile.js
+++ diary/static/js/profile.js
@@ -1,7 +1,6 @@
-
-window.onload = function(){
-    let token = localStorage.getItem('token');
-    let url = 'https://andela-diaryapi.herokuapp.com/api/v1/profile'
+let token = localStorage.getItem('token');
+let url = 'https://andela-diaryapi.herokuapp.com/api/v1/profile';
+window.onload = function(){    
     fetch(url, {
         headers: {
             'content-type': 'application/json',
@@ -14,7 +13,26 @@ window.onload = function(){
             document.querySelector('input[name=username]').value = data.username;
             document.querySelector('input[name=email]').value = data.email;
             document.querySelector('span#total_entries').innerText = data.total_entries;
-            console.log(data.name, data.email, data.username, data.total_entries);
+            // console.log(data.name, data.email, data.username, data.total_entries);
         })
         .catch(error => console.log(error));
 };
+
+function addReminder(){    
+    let reminder = document.querySelector('input[name=reminder]').checked;
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'x-access-token': token
+        },
+        body: JSON.stringify({
+            'reminder': reminder
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+    // console.log(reminder)
+}
